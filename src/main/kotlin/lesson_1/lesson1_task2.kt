3aeb79cbf4504053fe48942e0cf87d116cefcd09
Mad <uralsmart@gmail.com>
2025-02-03 21:00:59 +0500

    KS-1-2-variables-02

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val gratitudeText: String = "Thank you for purchase!"
+    var numberOfEmployees: Int = 2000
+
+    println(numberOfOrders)
+    println(gratitudeText)
+//    println(numberOfEmployees)
+
+    numberOfEmployees -= 1
+
+    println(numberOfEmployees)
+}
