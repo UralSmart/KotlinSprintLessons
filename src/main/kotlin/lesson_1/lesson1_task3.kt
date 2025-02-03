3635b99f8de3f4f9b66dbb65e5834710d2794819
Mad <uralsmart@gmail.com>
2025-02-03 21:17:58 +0500

    KS-1-3-variables

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,6 +1,16 @@
 package org.example.lesson_1
 
 fun main() {
-    println("Тестовое решение к уроку 1 закдаче 1")
-    println("какие-то доработки")
+    val year: Short = 1961
+    var hour: String = "09"
+    var minute: String = "07"
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+
+    println("$hour:$minute")
 }
