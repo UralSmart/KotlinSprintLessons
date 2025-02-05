f398b98d4c2c210b2316d335889ea04657f8ae0b
Mad <uralsmart@gmail.com>
2025-02-05 19:44:14 +0500

    KS-1-5-magic-number-fix

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -5,7 +5,7 @@ const val SECONDS_IN_MINUTE = 60
 fun main() {
     val secondsTotal: Int = 6360
 
-    val hours: Int = secondsTotal / 3600
+    val hours: Int = secondsTotal / (SECONDS_IN_MINUTE * SECONDS_IN_MINUTE)
     val minutes: Int = (secondsTotal - ((SECONDS_IN_MINUTE * SECONDS_IN_MINUTE) * hours)) / SECONDS_IN_MINUTE
     val seconds: Int = secondsTotal - ((SECONDS_IN_MINUTE * SECONDS_IN_MINUTE) * hours) - (SECONDS_IN_MINUTE * minutes)
 
